b82726ace8821deef57baba2445a5108347d5300
ndulue <mail2emekandulue@gmail.com>
2020-06-25 22:40:49 +0100

    update

---
+++ db.js
@@ -0,0 +1,11 @@
+var config = {
+    database: {
+        host: 'laundry',
+        user: 'root',
+        password: '',
+        port: '3306',
+        db: 'c2c'
+    }
+}
+
+module.exports = config;
